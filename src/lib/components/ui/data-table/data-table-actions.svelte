d03abb9c17d65cfa736b06663d20fefabfb274d7
NamPE286 <nambuihung654@gmail.com>
2025-11-18 10:07:50 +0700

    chore: add all shadcn component

---
+++ src/lib/components/ui/data-table/data-table-actions.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+	import EllipsisIcon from 'lucide-svelte/icons/ellipsis';
+	import { Button } from '$lib/components/ui/button';
+	import * as DropdownMenu from '$lib/components/ui/dropdown-menu';
+
+	type Props = {
+		id: number;
+	};
+
+	let { id }: Props = $props();
+</script>
+
+<DropdownMenu.Root>
+	<DropdownMenu.Trigger asChild let:builder>
+		<Button variant="ghost" builders={[builder]} size="icon" class="relative size-8 p-0">
+			<span class="sr-only">Open menu</span>
+			<EllipsisIcon class="size-4" />
+		</Button>
+	</DropdownMenu.Trigger>
+	<DropdownMenu.Content>
+		<DropdownMenu.Group>
+			<DropdownMenu.Label>Actions</DropdownMenu.Label>
+			<DropdownMenu.Item onclick={() => navigator.clipboard.writeText(id.toString())}>
+				Copy customer ID
+			</DropdownMenu.Item>
+		</DropdownMenu.Group>
+		<DropdownMenu.Separator />
+		<DropdownMenu.Item>View customer</DropdownMenu.Item>
+		<DropdownMenu.Item>View orders</DropdownMenu.Item>
+	</DropdownMenu.Content>
+</DropdownMenu.Root>
---
+++ src/lib/components/ui/data-table/data-table-name-button.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import ArrowUpDownIcon from 'lucide-svelte/icons/arrow-up-down';
+	import { Button } from '$lib/components/ui/button';
+
+	type Props = {
+		onclick?: (event: unknown) => void;
+	};
+
+	let { onclick }: Props = $props();
+</script>
+
+<Button variant="ghost" size="sm" class="-ml-3 h-8 data-[state=open]:bg-accent" {onclick}>
+	<span>Company Name</span>
+	<ArrowUpDownIcon class="ml-2 size-4" />
+</Button>
